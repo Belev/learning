cfe8078c4f47e3d9a5590c9887c6dd0e63f1c026
mbelev <martinbelev93@gmail.com>
2015-02-28 01:56:21 +0200

    Registered PostAdmin model for admin panel.

--- Python/django_blog/post/admin.py
+++ Python/django_blog/post/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from post.models import Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Post info', {
+            'classes': ['collapse'],
+            'fields': ['title', 'created_on', 'author']
+        }),
+        ('Post text', {
+            'classes': ['collapse'],
+            'fields': ['text']
+        }),
+        ('Post tags', {
+            'classes': ['collapse'],
+            'fields': ['tags']
+        })
+    ]
+    list_display = ['title', 'created_on', 'author']
+
+admin.site.register(Post, PostAdmin)
 # Register your models here.

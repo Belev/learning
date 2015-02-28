fb417b747718b690ecfb9889e9596f29e5a8d3f3
mbelev <martinbelev93@gmail.com>
2015-02-28 13:41:30 +0200

    Added names to django blog urls.

--- Python/django_blog/django_blog/urls.py
+++ Python/django_blog/django_blog/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 
 
 urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^post/', include('post.urls')),
 
-                       url(r'^$', 'django_blog.views.index'),
-                       url(r'^about/$', 'django_blog.views.about'),
-                       url(r'^contact/$', 'django_blog.views.contact'),
+                       url(r'^$', 'django_blog.views.index', name='index'),
+                       url(r'^about/$', 'django_blog.views.about', name='about'),
+                       url(r'^contact/$', 'django_blog.views.contact', name='contact'),
                        url(r'^\w+/$', 'django_blog.views.index'),
+
+                       url(r'^post/', include('post.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
                        )

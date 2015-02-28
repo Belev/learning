67495057b04d5d1c0d128ad38bfda29c673872c6
mbelev <martinbelev93@gmail.com>
2015-02-28 13:42:44 +0200

    Removed post index view name so that for index will be used the url from django_blog.

--- Python/django_blog/post/urls.py
+++ Python/django_blog/post/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
+
 from post import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
+                       url(r'^$', views.index),
                        url(r'^(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
                        )

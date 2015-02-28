62f16c51d694e8e80031f5e105020a4a4a98f551
mbelev <martinbelev93@gmail.com>
2015-02-28 17:38:35 +0200

    robots.txt url added for eventual searching by search engines.

--- Python/django_blog/django_blog/urls.py
+++ Python/django_blog/django_blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
 
 
 urlpatterns = patterns('',
@@ -7,8 +8,9 @@ urlpatterns = patterns('',
                        url(r'^$', 'django_blog.views.index', name='index'),
                        url(r'^about/$', 'django_blog.views.about', name='about'),
                        url(r'^contact/$', 'django_blog.views.contact', name='contact'),
-                       url(r'^\w+/$', 'django_blog.views.index'),
 
                        url(r'^post/', include('post.urls')),
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^robots\.txt$', lambda res: HttpResponse(
+                           "User-agent: *\nDisallow: ", content_type="text/plain"))
                        )

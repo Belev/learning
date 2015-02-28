c8f4d86334fb7037b5a31cef74ff766844a9b28c
mbelev <martinbelev93@gmail.com>
2015-02-28 11:45:37 +0200

    Added urls for moved templates.

--- Python/django_blog/django_blog/urls.py
+++ Python/django_blog/django_blog/urls.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^post/', include('post.urls')),
-                       url(r'^$', 'post.views.index'),
-                       url(r'^\w+/$', 'post.views.index'),
+
+                       url(r'^$', 'django_blog.views.index'),
+                       url(r'^about/$', 'django_blog.views.about'),
+                       url(r'^contact/$', 'django_blog.views.contact'),
+                       url(r'^\w+/$', 'django_blog.views.index'),
                        )

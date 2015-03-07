cd0f5d5c5b0c7b7622021e298d42fee99b27c33f
mbelev <martinbelev93@gmail.com>
2015-03-07 10:35:51 +0200

    Added registerData and modal.

--- JavaScript/IonicGallery/ionicGalleryApp/www/js/controllers.js
+++ JavaScript/IonicGallery/ionicGalleryApp/www/js/controllers.js
@@ -3,22 +3,23 @@ angular.module('starter.controllers', [])
     .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
         // Form data for the login modal
         $scope.loginData = {};
+        $scope.registerData = {};
 
-        // Create the login modal that we will use later
+        // Login modal - show, close, login
         $ionicModal.fromTemplateUrl('templates/login.html', {
             scope: $scope
         }).then(function (modal) {
-            $scope.modal = modal;
+            $scope.loginModal = modal;
         });
+
+        // Open the login modal
+        $scope.showLogin = function () {
+            $scope.loginModal.show();
+        };
 
         // Triggered in the login modal to close it
         $scope.closeLogin = function () {
-            $scope.modal.hide();
-        };
-
-        // Open the login modal
-        $scope.login = function () {
-            $scope.modal.show();
+            $scope.loginModal.hide();
         };
 
         // Perform the login action when the user submits the login form
@@ -29,6 +30,34 @@ angular.module('starter.controllers', [])
             // code if using a login system
             $timeout(function () {
                 $scope.closeLogin();
+            }, 1000);
+        };
+
+        // Register modal
+        $ionicModal.fromTemplateUrl('templates/register.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.registerModal = modal;
+        });
+
+        // Open the register modal
+        $scope.showSignup = function () {
+            $scope.registerModal.show();
+        };
+
+        // Triggered in the register modal to close it
+        $scope.closeSignup = function () {
+            $scope.registerModal.hide();
+        };
+
+        // Perform the register action when the user submits the login form
+        $scope.doSignup = function () {
+            console.log('Doing signup', $scope.registerData);
+
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeSignup();
             }, 1000);
         };
     })

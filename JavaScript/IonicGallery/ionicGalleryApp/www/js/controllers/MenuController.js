35318836fce9592bc84f40baee532b982dc57a98
mbelev <martinbelev93@gmail.com>
2015-03-07 18:58:56 +0200

    Changed controller name.

--- JavaScript/IonicGallery/ionicGalleryApp/www/js/controllers/MenuController.js
+++ JavaScript/IonicGallery/ionicGalleryApp/www/js/controllers/MenuController.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers')
-    .controller('MenuCtrl', function ($scope, $ionicSideMenuDelegate) {
+    .controller('MenuController', function ($scope, $ionicSideMenuDelegate) {
         $scope.toggleLeft = function () {
             $ionicSideMenuDelegate.toggleLeft();
         };

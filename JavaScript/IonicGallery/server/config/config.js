780a09ce116b3da720fc608f9fba7afe14bfc9d4
mbelev <martinbelev93@gmail.com>
2015-03-08 10:48:58 +0200

    Added openshift port and ip.

--- JavaScript/IonicGallery/server/config/config.js
+++ JavaScript/IonicGallery/server/config/config.js
@@ -3,7 +3,8 @@
 module.exports = {
     development: {
         db: 'mongodb://admin:admin@ds039291.mongolab.com:39291/ionic-gallery',
-        port: process.env.NODE_ENV || 8080,
+        port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
         session: {
             resave: true,
             saveUninitialized: true,
@@ -12,7 +13,8 @@ module.exports = {
     },
     production: {
         db: 'mongodb://admin:admin@ds039291.mongolab.com:39291/ionic-gallery',
-        port: process.env.NODE_ENV || 8081,
+        port: process.env.OPENSHIFT_NODEJS_PORT || 8081,
+        ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
         session: {
             resave: true,
             saveUninitialized: true,

15cd19f0457a1d8367124cd64a9a594d6bbdfeca
mbelev <martinbelev93@gmail.com>
2015-03-12 21:34:32 +0200

    User images now are being cached for offline usage.

--- JavaScript/IonicGallery/ionicGalleryApp/www/js/services/images.js
+++ JavaScript/IonicGallery/ionicGalleryApp/www/js/services/images.js
@@ -1,45 +1,65 @@
 angular.module('starter.services')
-    .factory('images', ['$http', '$q',
-        function ($http, $q) {
-            return {
-                getAllImagesForUser: function (userId) {
-                    var deferred = $q.defer();
-                    var requestOptions = {
-                        method: 'GET',
-                        url: 'http://ionicgalleryapi-mbelev.rhcloud.com/api/user/' + userId + '/images'
-                    };
+    .factory('images', ['$http', '$q', 'DSCacheFactory',
+        function ($http, $q, DSCacheFactory) {
+            var imagesCache = DSCacheFactory.get('imagesCache');
 
+            function getAllImagesForUser(userId, forceRefresh) {
+                var deferred = $q.defer();
+                var requestOptions = {
+                    method: 'GET',
+                    url: 'http://ionicgalleryapi-mbelev.rhcloud.com/api/user/' + userId + '/images'
+                };
+
+                var cacheKey = 'imagesCache-' + userId;
+                var userImages = null;
+
+                if (!forceRefresh) {
+                    userImages = imagesCache.get(cacheKey);
+                }
+
+                if (userImages) {
+                    console.log('Get images from cache.');
+                    deferred.resolve(userImages);
+                } else {
                     $http(requestOptions)
                         .success(function (data, status, headers, config) {
+                            console.log('Get images from server.');
+                            imagesCache.put(cacheKey, data);
                             deferred.resolve(data);
                         })
                         .error(function (data, status, headers, config) {
                             deferred.reject();
                         });
-
-                    return deferred.promise;
-                },
-                saveUserImage: function (userId, imageSrc) {
-                    var deferred = $q.defer();
-                    console.log(userId + ' ' + imageSrc);
-
-                    var requestOptions = {
-                        method: 'POST',
-                        url: 'http://ionicgalleryapi-mbelev.rhcloud.com/api/user/' + userId + '/saveImage',
-                        data: {
-                            imageSrc: imageSrc
-                        }
-                    };
-
-                    $http(requestOptions)
-                        .success(function (data, status, headers, config) {
-                            deferred.resolve();
-                        })
-                        .error(function (data, status, headers, config) {
-                            deferred.reject();
-                        });
-
-                    return deferred.promise;
                 }
+
+                return deferred.promise;
+            }
+
+            function saveUserImage(userId, imageSrc) {
+                var deferred = $q.defer();
+                console.log(userId + ' ' + imageSrc);
+
+                var requestOptions = {
+                    method: 'POST',
+                    url: 'http://ionicgalleryapi-mbelev.rhcloud.com/api/user/' + userId + '/saveImage',
+                    data: {
+                        imageSrc: imageSrc
+                    }
+                };
+
+                $http(requestOptions)
+                    .success(function (data, status, headers, config) {
+                        deferred.resolve();
+                    })
+                    .error(function (data, status, headers, config) {
+                        deferred.reject();
+                    });
+
+                return deferred.promise;
+            }
+
+            return {
+                getAllImagesForUser: getAllImagesForUser,
+                saveUserImage: saveUserImage
             }
         }]);

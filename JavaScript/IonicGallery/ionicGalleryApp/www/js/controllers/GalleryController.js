a2f0651b15347e24f0bf940a69ed8f37f3669775
mbelev <martinbelev93@gmail.com>
2015-03-12 21:36:59 +0200

    Uses forceRefresh to know when to get images from cache and when to make a server call.

--- JavaScript/IonicGallery/ionicGalleryApp/www/js/controllers/GalleryController.js
+++ JavaScript/IonicGallery/ionicGalleryApp/www/js/controllers/GalleryController.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers')
     .controller('GalleryController', ['$scope', 'images', 'identity', function ($scope, images, identity) {
-        function getUserImages(userId) {
-            images.getAllImagesForUser(userId)
+        function getUserImages(userId, forceRefresh) {
+            images.getAllImagesForUser(userId, forceRefresh)
                 .then(function (userImages) {
                     $scope.images = userImages;
                     console.log($scope.images);
@@ -17,7 +17,7 @@ angular.module('starter.controllers')
             getUserImages(currentUser._id);
         }
 
-        $scope.updateGalleryImages = function () {
-            getUserImages(currentUser._id);
+        $scope.updateGalleryImages = function (forceRefresh) {
+            getUserImages(currentUser._id, forceRefresh);
         };
     }]);

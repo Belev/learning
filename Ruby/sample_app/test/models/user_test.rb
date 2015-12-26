a64faaf173fa086d5082edb6e79b5d20bab5b2b1
Martin Belev <martinbelev93@gmail.com>
2015-12-26 17:55:30 +0200

    Add feed functionality

--- Ruby/sample_app/test/models/user_test.rb
+++ Ruby/sample_app/test/models/user_test.rb
@@ -70,4 +70,27 @@ class UserTest < ActiveSupport::TestCase
     user.unfollow(user2)
     assert_not user.following?(user2)
   end
+
+  test "feed should have the right posts" do
+    user = users(:user)
+    user2 = users(:user2)
+    user3 = users(:user3)
+
+    user.follow(user3)
+
+    # Posts from self
+    user.microposts.each do |post_self|
+      assert user.feed.include?(post_self)
+    end
+
+    # Posts from followed user
+    user3.microposts.each do |post_following|
+      assert user.feed.include?(post_following)
+    end
+
+    # Posts from unfollowed user
+    user2.microposts.each do |post_unfollowed|
+      assert_not user.feed.include?(post_unfollowed)
+    end
+  end
 end

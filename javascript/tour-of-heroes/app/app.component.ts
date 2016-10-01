836fcceff5864b428c437c612f4b287e747f294b
Martin Belev <martinbelev93@gmail.com>
2016-10-01 15:13:46 +0300

    Add simple hero model

--- javascript/tour-of-heroes/app/app.component.ts
+++ javascript/tour-of-heroes/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {Hero} from "./models/hero";
 
 @Component({
   selector: 'my-app',
   templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}

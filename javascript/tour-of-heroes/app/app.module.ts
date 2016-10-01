0849f16a358b021bd5163626905763950d9d1a99
Martin Belev <martinbelev93@gmail.com>
2016-10-01 18:32:22 +0300

    Add http and in memory web api modules

--- javascript/tour-of-heroes/app/app.module.ts
+++ javascript/tour-of-heroes/app/app.module.ts
@@ -7,11 +7,16 @@ import {HeroesComponent} from "./hero/heroes.component";
 import {HeroDetailsComponent} from "./hero/hero-details.component";
 import {routing} from "./app.routing";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HttpModule} from "@angular/http";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./services/in-memory-data.service";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [

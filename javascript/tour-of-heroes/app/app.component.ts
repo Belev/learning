1d2a60885509ee0d45e9b8fe77e1ba3f3f732313
Martin Belev <martinbelev93@gmail.com>
2016-10-01 15:29:29 +0300

    Display heroes and add hero selection

--- javascript/tour-of-heroes/app/app.component.ts
+++ javascript/tour-of-heroes/app/app.component.ts
@@ -1,14 +1,34 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Hero} from "./models/hero";
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app/app.component.html'
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css']
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+export class AppComponent implements OnInit {
+  title: string;
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  ngOnInit(): void {
+    this.title = 'Tour of Heroes';
+
+    this.heroes = [
+      { id: 1, name: 'Mr. Nice' },
+      { id: 2, name: 'Narco' },
+      { id: 3, name: 'Bombasto' },
+      { id: 4, name: 'Celeritas' },
+      { id: 5, name: 'Magneta' },
+      { id: 6, name: 'RubberMan' },
+      { id: 7, name: 'Dynama' },
+      { id: 8, name: 'Dr IQ' },
+      { id: 9, name: 'Magma' },
+      { id: 10, name: 'Tornado' }
+    ];
+  }
+
+  onSelectHero(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }

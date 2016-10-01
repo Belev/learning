5c0a01df3bb6f01e1bd2c7d727ad8d5335523063
Martin Belev <martinbelev93@gmail.com>
2016-10-01 17:55:59 +0300

    Complete the Angular 2 Tour of Heroes tutorial
     * Add routing
     * Add dashboard component
     * Refine hero details
     * Add styles

--- javascript/tour-of-heroes/app/app.module.ts
+++ javascript/tour-of-heroes/app/app.module.ts
@@ -5,14 +5,18 @@ import {AppComponent} from "./app.component";
 import {HeroService} from "./services/hero.service";
 import {HeroesComponent} from "./hero/heroes.component";
 import {HeroDetailsComponent} from "./hero/hero-details.component";
+import {routing} from "./app.routing";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailsComponent,
     HeroesComponent
   ],
--- javascript/tour-of-heroes/app/hero/heroes.component.ts
+++ javascript/tour-of-heroes/app/hero/heroes.component.ts
@@ -1,17 +1,22 @@
 import {Component, OnInit} from "@angular/core";
 import {HeroService} from "../services/hero.service";
 import {Hero} from "../models/hero";
+import {Router} from "@angular/router";
 
 @Component({
+  moduleId: module.id,
   selector: 'heroes',
-  templateUrl: './app/hero/heroes.component.html',
-  styleUrls: ['./app/hero/heroes.component.css']
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -19,5 +24,9 @@ export class HeroesComponent implements OnInit {
 
   onSelectHero(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  viewDetails(): void {
+    this.router.navigate(['/heroes', this.selectedHero.id]);
   }
 }

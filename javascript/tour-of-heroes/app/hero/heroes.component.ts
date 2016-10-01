d12f57f1c4668d6ed882a0dc604ecddf07c734dc
Martin Belev <martinbelev93@gmail.com>
2016-10-01 18:53:57 +0300

    Add delete hero support

--- javascript/tour-of-heroes/app/hero/heroes.component.ts
+++ javascript/tour-of-heroes/app/hero/heroes.component.ts
@@ -35,6 +35,16 @@ export class HeroesComponent implements OnInit {
       });
   }
 
+  delete(hero: Hero): void {
+    this.heroService.delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h.id !== hero.id);
+        if (this.selectedHero === hero) {
+          this.selectedHero = null;
+        }
+      });
+  }
+
   onSelectHero(hero: Hero): void {
     this.selectedHero = hero;
   }

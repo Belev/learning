9d5908d24e56812aae1f1f986dc4bbf3bef5fb44
Martin Belev <martinbelev93@gmail.com>
2016-10-02 14:25:18 +0300

    Prepare to deploy on heroku

--- ruby/sample_app/app/controllers/users_controller.rb
+++ ruby/sample_app/app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.activate()
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      log_in @user
+      redirect_to @user
     else
       render 'new'
     end
